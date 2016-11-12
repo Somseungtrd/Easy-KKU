19be2439ad09f2f308ad9122a2cf9228c8b2a180
somseung <somseungxxi@gmail.com>
2016-11-12 16:00:11 +0700

    Image Path

--- app/src/main/java/com/example/namsom/easykku/SignUpActivity.java
+++ app/src/main/java/com/example/namsom/easykku/SignUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.namsom.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,7 +21,7 @@ public class SignUpActivity extends AppCompatActivity {
             userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString, imagePathString, imageNameString;
     private Uri uri;
 
 
@@ -100,9 +102,34 @@ public class SignUpActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            //find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagePath ==>" + imagePathString);
+
+
 
         }   // if
 
     }   // onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor != null) {
+
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        } else {
+            result = uri.getPath();
+        }
+
+
+        return result;
+    }
 
 }   // Main Class
